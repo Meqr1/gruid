c6771a2df930191696e2279effefffe48fb6db60
M Query <135724390+Meqr1@users.noreply.github.com>
2024-07-21 20:56:14 +0530

    init commit

---
+++ include/helloFrame.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_FRAME_H
+#define HELLO_FRAME_H
+
+void say_hello(void);
+
+#endif // HELLO_FRAME_H
---
+++ src/helloFrame.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "helloFrame.h"
+
+void say_hello(void) {
+  printf("hello, World!\n");
+}
